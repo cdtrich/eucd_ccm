7d9653298a43a0c270087bf181c679f1e8d92f04
cd_trich <31612691+cdtrich@users.noreply.github.com>
2020-08-13 17:53:26 +0200

    all scripts created and dyads network badly working

--- js/5_dyads.js
+++ js/5_dyads.js
@@ -20,12 +20,16 @@ import {
 	timeFormat,
 	scaleOrdinal,
 	line as _line,
-	forceSimulation
+	forceSimulation,
+	forceLink,
+	forceManyBody,
+	forceX,
+	forceY
 } from "d3";
 
 // import _ from "lodash";
 // Load the core build.
-import { chain, replace, split } from "lodash";
+import { chain, replace, split, uniqBy, filter } from "lodash";
 
 // import fetch as d3-fetch from "d3-fetch";
 import { csv } from "d3-fetch";
@@ -107,18 +111,33 @@ csv(url, (d) => {
 
 	// x - dyad countries
 	//// unique countries
-	const nodes = chain(data)
+	var nodes = data;
+	const links = data;
+	// console.log(nodes);
+
+	var nodesFromUnique = chain(nodes)
+		.uniqBy((d) => d.dyad_from)
+		.value();
+	// console.log(nodesFromUnique);
+
+	var nodesToUnique = chain(nodes)
+		.uniqBy((d) => d.dyad_to)
+		.value();
+	// console.log(nodesToUnique);
+
+	var nodesUnique = chain(nodes)
 		.map((d) => [d.dyad_from, d.dyad_to])
 		.flatten()
 		.uniq()
 		.value();
+	// console.log(nodesUnique);
+
+	// const obj = nodesUnique.reduce((array, value, index) => ({ ...array, [index]: value }), {});
+
+	nodes = nodesUnique.map((d) => {
+		return { id: d };
+	});
 	console.log(nodes);
-
-	const links = data;
-	console.log(links);
-
-	// const links = chain(data).map((d) => [d.dyad_from, d.dyad_to]).value();
-	// console.log(links);
 
 	// color - Existence_of_Cyber_Command
 	//// unique types
@@ -138,6 +157,75 @@ csv(url, (d) => {
 
 	// const links = data.links.map(d => data.create(d));
 	// const nodes = data.nodes.map(d => nodes.create(d));
+
+	var simulation = forceSimulation(nodes)
+		.force(
+			"links",
+			forceLink(links).id((d) => d.dyad_from)
+		)
+		.force("charge", forceManyBody())
+		.force("x", forceX())
+		.force("y", forceY());
+
+	const link = svg
+		.append("g")
+		// .attr("stroke", "#999")
+		// .attr("stroke-opacity", 0.6)
+		.selectAll("line")
+		.data(links)
+		.join("line")
+		.attr("stroke", (d) => d.command);
+	// .attr("stroke-width", (d) => Math.sqrt(d.value));
+
+	const node = svg
+		.append("g")
+		.attr("stroke", "#fff")
+		.attr("stroke-width", 1.5)
+		.selectAll("circle")
+		.data(nodes)
+		.join("circle")
+		.attr("r", 5)
+		.attr("fill", "black");
+	// .call(drag(simulation));
+
+	node.append("title").text((d) => d.id);
+
+	simulation.on("tick", () => {
+		link
+			.attr("x1", (d) => d.source.x)
+			.attr("y1", (d) => d.source.y)
+			.attr("x2", (d) => d.target.x)
+			.attr("y2", (d) => d.target.y);
+
+		node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+	});
+
+	// invalidation.then(() => simulation.stop());
+
+	// var drag = function (simulation) {
+	// 	function dragstarted(d) {
+	// 		if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+	// 		d.fx = d.x;
+	// 		d.fy = d.y;
+	// 	}
+
+	// 	function dragged(d) {
+	// 		d.fx = d3.event.x;
+	// 		d.fy = d3.event.y;
+	// 	}
+
+	// 	function dragended(d) {
+	// 		if (!d3.event.active) simulation.alphaTarget(0);
+	// 		d.fx = null;
+	// 		d.fy = null;
+	// 	}
+
+	// 	return d3
+	// 		.drag()
+	// 		.on("start", dragstarted)
+	// 		.on("drag", dragged)
+	// 		.on("end", dragended);
+	// };
 
 	// dots
 	// const dots = svg
